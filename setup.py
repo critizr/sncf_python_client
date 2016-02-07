3ae37cb64480dac90bc6cc08c91826980cddba16
Thibaut Carlier <thibaut@critizr.com>
2016-02-07 23:36:36 +0100

    correct copyrights

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   packages = ['sncf_api'], # this must be the same as the name above
   version = '0.1',
   description = 'A python client for SNCF API',
-  author = 'Thibaut Carlier',
+  author = 'Critizr',
   author_email = 'thibaut@critizr.com',
   url = 'todo', # use the URL to the github repo
   download_url = 'todo', # I'll explain this in a second
